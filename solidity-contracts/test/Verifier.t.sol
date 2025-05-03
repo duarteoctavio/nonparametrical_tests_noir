95cae4760eee7ca6cf37928bde9f3cdb81b347dd
calvo.generico <miguelfeliped@gmail.com>
2025-05-03 08:53:41 -0300

    verifier test working again

--- solidity-contracts/test/Verifier.t.sol
+++ solidity-contracts/test/Verifier.t.sol
@@ -7,18 +7,47 @@ import {HonkVerifier} from "../src/Verifier.sol";
 
 contract VerifierTest is Test {
     HonkVerifier public verifier;
-    bytes public proof;
-    bytes public pInputs;
+    bytes public fullProof;
     bytes32[] public inputs;
+    uint256 constant public PUBLIC_INPUTS_LENGTH = 1;
 
     function setUp() public {
         verifier = new HonkVerifier();
-        proof = vm.readFileBinary("./src/clean_proof");
-        pInputs = vm.readFileBinary("./src/clean_public_inputs");
+        fullProof = vm.readFileBinary("./test/proof");
+    }
+
+    function test_verifier() public view {
+        (bytes memory proof, bytes32[] memory pubInputs) = splitProof(fullProof);
+        
+        verifier.verify(proof, pubInputs);
+    }
+
+    function splitProof(bytes memory fullProofBytes) private pure returns (bytes memory proof, bytes32[] memory pubInputs) {
+        // First remove the first 4 bytes with the length
+        bytes memory proofSlice = new bytes(fullProofBytes.length - 4);
+        for(uint i = 4; i < fullProofBytes.length; i++) {
+            proofSlice[i - 4] = fullProofBytes[i];
+        }
+
+        // Then convert the bytes to bytes32[]
+        bytes32[] memory fullProofWords = bytesToBytes32Array(proofSlice);
+        
+        // Then extract the public inputs
+        pubInputs = new bytes32[](PUBLIC_INPUTS_LENGTH);
+        for(uint i = 0; i < PUBLIC_INPUTS_LENGTH; i++) {
+            pubInputs[i] = fullProofWords[i];
+        }
+
+        // Lastly recovert the proof to bytes
+        bytes32[] memory proofWords = new bytes32[](fullProofWords.length - 1);
+        for(uint i = 1; i < fullProofWords.length; i++) {
+            proofWords[i - 1] = fullProofWords[i];
+        }
+        proof = bytes32ArrayToBytes(proofWords);
     }
 
     function bytesToBytes32Array(bytes memory data)
-        public
+        private
         pure
         returns (bytes32[] memory)
     {
@@ -43,16 +72,17 @@ contract VerifierTest is Test {
         return (dataList);
     }
 
-    function test_readFile() public {
-        bytes memory content = vm.readFileBinary("./some-file.txt");
-        assertEq(content[0], 'h');
-        assertEq(content[1], 'o');
-        assertEq(content[2], 'l');
-        assertEq(content[3], 'a');
-    }
-
-    function test_verifier() public {
-        inputs = bytesToBytes32Array(pInputs);
-        verifier.verify(proof, inputs);
+    function bytes32ArrayToBytes(bytes32[] memory data)
+        private
+        pure
+        returns (bytes memory)
+    {
+        bytes memory result = new bytes(data.length * 32);
+        for(uint i = 0; i < data.length; i++) {
+            assembly {
+                mstore(add(result, add(32, mul(i, 32))), mload(add(data, add(32, mul(i, 32)))))
+            }
+        }
+        return result;
     }
 }
