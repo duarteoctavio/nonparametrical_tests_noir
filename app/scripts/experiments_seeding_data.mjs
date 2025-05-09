e92abc257256058250747eb4d59d3fd52e98a3e8
Octavio Martín Duarte <duarteoctaviomartin@gmail.com>
2025-05-09 17:51:09 -0300

    added 5 seed experiments

---
+++ app/scripts/experiments_seeding_data.mjs
@@ -0,0 +1,34 @@
+const { createExperiment } = require("../app/.server/dto/experiments");
+
+const seedExperiments = [
+    {
+        title: "Mid term effectivity of candidate drug #S632.",
+        description: "Drug candidate #S632 is a promising cure to Malaria accordint to two propnentes, but their financial motivation as patent owners and manufacturers disqualifies them for final demonstration. The protocol is public, avaible in DOI:23523.",
+        bounty: 1000000,
+        image: "durgCandidate.jpg",
+    },
+    {
+        title: "Flow dynamics of toroidal drug bomb are above spec.",
+        description: "A pre approved drug bomb has several advantages but its flow dynamics behaviour is still not backed by conclussive results.",
+        bounty: 70000,
+        image: "drugPump.jpg",
+    },
+    {
+        title: "Correlation between exposition to microplastis and Alzheimer",
+        description: "A national entity has gathered substantial evidence in this direction andneeds to bolster its argument. Revalidator needs to build a sizeable dataset and measure with the same or more precission as indicated in DOI:111222.",
+        bounty: 100000,
+        image: "microplastics.jpg"
+    },
+    {
+        title: "Aggresion in schoolchildren driven down by smartphone usage restriction.",
+        description: "DOI:11374 has been widely recognized and is substantially backed by evidence, but all of it is based in nations too culturally different to Estonia, where a school is interested in replicating. Revalidator should run replication across at least 50 schools in baltic nations.",
+        bounty: 2010351,
+        image: "estonianSchool.jpg"
+    },
+    {
+        title: "Tensile strenght of S235",
+        description: "Steel provided to a construction company by a new provider needs to be evealuated by  neutral third party.",
+        bounty:3500,
+        image:"tensileStrength.jpg",
+    },
+];
